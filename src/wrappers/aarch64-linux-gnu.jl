c09bde4cefe30f5a506871bc888dc8f1020b202f
jlbuild <juliabuildbot@gmail.com>
2019-12-18 00:35:41 +0000

    Xorg_xkeyboard_config_jll build 2.27.0+1

--- src/wrappers/aarch64-linux-gnu.jl
+++ src/wrappers/aarch64-linux-gnu.jl
@@ -9,12 +9,16 @@ LIBPATH_env = "LD_LIBRARY_PATH"
 Open all libraries
 """
 function __init__()
-    global prefix = abspath(joinpath(@__DIR__, ".."))
+    global artifact_dir = abspath(artifact"Xorg_xkeyboard_config")
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    append!.(Ref(PATH_list), (Xorg_xkbcomp_jll.PATH_list,))
-    append!.(Ref(LIBPATH_list), (Xorg_xkbcomp_jll.LIBPATH_list,))
+    # We first need to add to LIBPATH_list the libraries provided by Julia
+    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
+    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
+    # then append them to our own.
+    foreach(p -> append!(PATH_list, p), (Xorg_xkbcomp_jll.PATH_list,))
+    foreach(p -> append!(LIBPATH_list, p), (Xorg_xkbcomp_jll.LIBPATH_list,))
 
     # Filter out duplicate and empty entries in our PATH and LIBPATH entries
     filter!(!isempty, unique!(PATH_list))
